e71a8d16fb64a05a53989002b87d6d11200a4c1d
xiao ching <carol.xiao.ching@gmail.com>
2025-08-01 18:41:27 +0800

    feat: 🎸 新增 imageAPI

---
+++ apis/image.js
@@ -0,0 +1,32 @@
+export const imageAPI = () => {
+  const {
+    public: { apiUrl },
+  } = useRuntimeConfig();
+  const authToken = ref("");
+
+  function apiUploadImages(data, type = "photo") {
+    return $fetch(`api/images?type=${type}`, {
+      baseURL: apiUrl,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authToken.value}`,
+      },
+      body: data,
+    });
+  }
+
+  function apiDeleteImages(imageId) {
+    return $fetch(`api/images/${imageId}`, {
+      baseURL: apiUrl,
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${authToken.value}`,
+      },
+    });
+  }
+
+  return {
+    apiDeleteImages,
+    apiUploadImages,
+  };
+};
