a8e9c795a4785e47286831e706ef38b15fe772e5
xiao ching <carol.xiao.ching@gmail.com>
2025-08-03 11:10:59 +0800

    feat: 🎸 新增 vue-loading-overlay 套件並設定 loading 功能

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <script setup>
 // Use SSR-safe IDs for Headless UI
 provideHeadlessUseId(() => useId());
+
+const { isLoading } = useLoading();
 </script>
 
 <template>
@@ -11,6 +13,15 @@ provideHeadlessUseId(() => useId());
     <ClientOnly>
       <BaseToastMessage />
       <BaseAlertLoginModal />
+      <Loading
+        :active="isLoading"
+        :is-full-page="true"
+        :height="128"
+        :width="128"
+        loader="dots"
+        color="#D9BAA6"
+        background-color="#000"
+      />
     </ClientOnly>
   </NuxtLayout>
 </template>
