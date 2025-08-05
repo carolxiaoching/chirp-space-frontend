ffe1b773aa5978297dd021d538621993b6a9d144
xiao ching <carol.xiao.ching@gmail.com>
2025-08-05 11:53:56 +0800

    feat: 🎸 新增 個人資料設定頁面

---
+++ components/setting/UpdateAccount.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <div>UpdateAccount</div>
+</template>
+<style scoped></style>
---
+++ components/setting/UpdatePassword.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <div>UpdatePassword</div>
+</template>
+<style scoped></style>
