78b2b01e3e0f125b2e0b768c96ccdbeb2e1ec05f
carolxiaoching <carol.xiao.ching@gmail.com>
2026-04-23 11:28:49 +0800

    fix: 🐛 修復 Offcanvas 在伺服器端設定樣式錯誤問題

--- composables/useOffcanvas.js
+++ composables/useOffcanvas.js
@@ -4,13 +4,19 @@ export const useOffcanvas = () => {
   // 開啟 Offcanvas
   function openOffcanvas() {
     isOffcanvasOpen.value = true;
-    document.body.style.overflow = "hidden";
+    // 伺服器端不存在 document，需確認為客戶端才執行
+    if (import.meta.client) {
+      document.body.style.overflow = "hidden";
+    }
   }
 
   // 關閉 Offcanvas
   function closeOffcanvas() {
     isOffcanvasOpen.value = false;
-    document.body.style.overflow = "";
+    // 伺服器端不存在 document，需確認為客戶端才執行
+    if (import.meta.client) {
+      document.body.style.overflow = "";
+    }
   }
 
   return {
